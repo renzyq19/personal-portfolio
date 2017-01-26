4adb88b8f6d73665cfa61b2db1f41b1f26207716
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-01-26 15:05:43 +0000

    Initial commit, starting at lesson 9 of URBWA

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <Title text="123456c" />;
+  }
+}
+
+const Title = props => <h1> Title: {props.text}</h1>;
+
+Title.propTypes = {
+  text(props, propName) {
+    if (!(propName in props)) {
+      return new Error(`missing ${propName}`);
+    }
+    if (props[propName].length < 6) {
+      return new Error(`${propName} was too short`);
+    }
+
+  },
+};
+export default App;
