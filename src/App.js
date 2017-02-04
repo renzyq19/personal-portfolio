c864b4fdd1da9982dbe428f1be09a0224eee3349
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-02-04 16:52:37 +0000

    abandoning until we get back to bootstrap

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import React from 'react';
 const App = () => (
   <div id="main">
     <NavBar />
+    <Main />
   </div>
 );
 
 const NavBar = () => (
   <nav className="navbar">
-    <div className="container-fluid">
-      HELLO
-    </div>
+    <div className="container-fluid" />
   </nav>
+);
+
+const Main = () => (
+  <div className="container">
+    <div className="jumbotron">
+      Main Section
+    </div>
+  </div>
 );
 
 export default App;
