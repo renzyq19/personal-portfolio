4d3eed258736edd55bd785be039a765b8a961366
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-02-19 20:23:52 +0000

    moved navbar to separate file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // eslint no-unused-vars:false
 import React from 'react';
-import logo from './guildhall-logo.svg';
+import NavBar from './NavBar';
 
 const App = () => (
   <div id="main">
@@ -8,48 +8,7 @@ const App = () => (
     <Main />
   </div>
 );
-const NavBar = () => (
-  <nav className="navbar navbar-toggleable navbar-light navbar-fixed-top bg-faded">
-    <div className="container">
-      <button
-        className="navbar-toggler navbar-toggler-right" type="button"
-        data-toggle="collapse" data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <a className="navbar-brand" href="#1">
-        <img src={logo} width={100} height={100} alt="logo" />
-        Will de Renzy-Martin
-      </a>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="#about">
-            About <span className="sr-only">(current)</span>
-          </a>
-          <a className="nav-item nav-link" href="#portfolio">Portfolio</a>
-          <a className="nav-item nav-link" href="#contact">Contact</a>
-        </div>
-        <div className="navbar-nav ml-auto">
-          <div className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle" href="#dropdown"
-              id="navbarDropdownMenuLink" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a className="dropdown-item" href="#acting">Acting</a>
-              <a className="dropdown-item" href="#tutoring">Tutoring</a>
-              <a className="dropdown-item" href="#webdesign">Web Design</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </nav>
-);
+
 
 const Main = () => (
   <div className="container">
