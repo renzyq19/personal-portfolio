a298c6af711ee1104c1285b3574e33b47d7e7e40
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-03-15 14:30:51 +0000

    temp navbar fix for upload

--- src/NavBar.js
+++ src/NavBar.js
@@ -12,35 +12,47 @@ const NavBar = () => (
         <span className="navbar-toggler-icon" />
       </button>
       <a className="navbar-brand" href="#about">
-        <img src={logo} width={100} height={100} alt="logo" />
+        <img src={logo} width={75} height={75} alt="logo" />
       </a>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
           <a className="nav-item nav-link active" href="#about">
             About <span className="sr-only">(current)</span>
           </a>
-          <a className="nav-item nav-link" href="#portfolio">Portfolio</a>
-          <a className="nav-item nav-link" href="#contact">Contact</a>
-        </div>
-        <div className="navbar-nav ml-auto">
-          <div className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle" href="#dropdown"
-              id="navbarDropdownMenuLink" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false"
-            >
-              Section
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a className="dropdown-item" href="#acting">Acting</a>
-              <a className="dropdown-item" href="#tutoring">Tutoring</a>
-              <a className="dropdown-item" href="#webdesign">Web Design</a>
-            </div>
-          </div>
         </div>
       </div>
     </div>
   </nav>
+);
+
+const Dump = () => (
+  <div>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <a className="nav-item nav-link active" href="#about">
+          About <span className="sr-only">(current)</span>
+        </a>
+        <a className="nav-item nav-link" href="#portfolio">Portfolio</a>
+        <a className="nav-item nav-link" href="#contact">Contact</a>
+      </div>
+      <div className="navbar-nav ml-auto">
+        <div className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle" href="#dropdown"
+            id="navbarDropdownMenuLink" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false"
+          >
+            Section
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a className="dropdown-item" href="#acting">Acting</a>
+            <a className="dropdown-item" href="#tutoring">Tutoring</a>
+            <a className="dropdown-item" href="#webdesign">Web Design</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 );
 
 export default NavBar;
