7e6f1f1eb1d7622c1c590e4e5aa1b1e0c4f57a22
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2017-06-08 15:12:51 +0100

    got bored trying to make onclick handler for progress

--- src/Voice.js
+++ src/Voice.js
@@ -2,16 +2,34 @@ import React from 'react';
 import voicereel from './voicereel.mp3';
 import './Voice.css';
 
-const Voice = () => (
-  <div className="main" >
-    <div className="box" >
-      <audio src={voicereel} />
-      <div className="player">
-        <div className="progressBar">
-          <div className="progress" />
+class Voice extends React.Component {
+  handleProgressUpdate() {
+  }
+  render() {
+    return (
+      <div className="main" >
+        <div className="box" >
+          <audio src={voicereel} />
+          <div className="player">
+            <button className="play_button">
+              <i className="material-icons md-48">play_arrow</i>
+            </button>
+            <ProgressBar onClick={this.handleProgressUpdate} />
+            <input
+              type="range" name="volume" className="player_slider"
+              min="0" max="1" step="0.1"
+              defaultValue="1"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    );
+  }
+}
+
+const ProgressBar = (props) => (
+  <div className="progress_bar" onClick={props.onClick}>
+    <div className="progress" />
   </div>
 );
 
